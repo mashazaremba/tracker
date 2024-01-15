f497d93d041c2463073e49493b93ead9a3118365
Maria <zarembamasha@icloud.com>
2024-01-15 21:51:38 +0300

    7.6 cat

--- src/main/java/ru/oop/Cat.java
+++ src/main/java/ru/oop/Cat.java
@@ -1,7 +1,40 @@
 package ru.oop;
 
 public class Cat {
-    public static void main(String[] args) {
-        Cat peppy = new Cat();
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.food);
+        System.out.println(this.name);
     }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Gav");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("Black");
+        black.show();
+        System.out.println("There are pushok's food.");
+        Cat pushok = new Cat();
+        pushok.eat("cheese");
+        pushok.giveNick("Pushok");
+        pushok.show();
+    }
+
+
 }
