b58e716434444c64ccb07915dd70dd659a9e8565
Maria <zarembamasha@icloud.com>
2024-02-13 20:47:20 +0300

    9.3 library

--- src/main/java/ru/pojo/Library.java
+++ src/main/java/ru/pojo/Library.java
@@ -1,30 +1,43 @@
 package ru.pojo;
 
 public class Library {
+
+    public static void printBooks(Book[] books) {
+        for (int i = 0; i < books.length; i++) {
+            Book b = books[i];
+            System.out.println(b.getName() + b.getPages());
+        }
+    }
+
+    public static void printByName(Book[] books, String bookName) {
+        for (int i = 0; i < books.length; i++) {
+            Book b = books[i];
+            if (b.getName().equals(bookName)) {
+                System.out.println(b.getName() + b.getPages());
+            }
+        }
+    }
+
+    public static void swap(Book[] books, int source, int dest) {
+        Book temp = books[source];
+        books[source] = books[dest];
+        books[dest] = temp;
+    }
+
     public static void main(String[] args) {
         Book book = new Book("Clean code", 50);
         Book book1 = new Book("Book1", 100);
         Book book2 = new Book("Book2", 200);
         Book book3 = new Book("Book3", 300);
         Book[] books = {book, book1, book2, book3};
-        for (int i = 0; i < books.length; i++) {
-           Book b = books[i];
-            System.out.println(b.getName() + b.getPages());
-        }
+        printBooks(books);
         System.out.println("______________");
-        for (int i = 0; i < books.length; i++) {
-            Book b = books[i];
-            if (b.getName().equals("Clean code")) {
-                System.out.println(b.getName() + b.getPages());
-            }
-        }
+        String bookName = "Clean code";
+        printByName(books, bookName);
         System.out.println("______________");
-        Book temp = books[0];
-        books[0] = books[3];
-        books[3] = temp;
-        for (int i = 0; i < books.length; i++) {
-            Book b = books[i];
-            System.out.println(b.getName() + b.getPages());
-        }
+        int source = 0;
+        int dest = 3;
+        swap(books, source, dest);
+        printBooks(books);
     }
 }
