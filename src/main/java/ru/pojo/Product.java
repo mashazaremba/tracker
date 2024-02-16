d1c3414ac000e92a02111f5f5266290e9632b23d
Maria <zarembamasha@icloud.com>
2024-02-16 22:23:36 +0300

    9.5 shopdrop

--- src/main/java/ru/pojo/Product.java
+++ src/main/java/ru/pojo/Product.java
@@ -24,4 +24,12 @@ public class Product {
     public void setCount(int count) {
         this.count = count;
     }
+
+    @Override
+    public String toString() {
+        return "Product{"
+                + "name='" + name + '\''
+                + ", count=" + count
+                + '}';
+    }
 }
