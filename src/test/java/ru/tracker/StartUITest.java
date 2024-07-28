b80264dfceb5ac9701cbabbf0c8a8204053016a3
Maria <zarembamasha@icloud.com>
2024-07-28 21:40:24 +0300

    9.12 toString

--- src/test/java/ru/tracker/StartUITest.java
+++ src/test/java/ru/tracker/StartUITest.java
@@ -1,5 +1,52 @@
 package ru.tracker;
 
+import org.junit.jupiter.api.Test;
+import ru.tracker.action.*;
+import ru.tracker.input.Input;
+import ru.tracker.input.MockInput;
+import ru.tracker.model.Item;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 class StartUITest {
+    @Test
+    void whenCreateItem() {
+        Input input = new MockInput(new String[]{"0", "Item name", "1"});
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(),
+                new ExitAction()
+        };
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
+    }
+
+    @Test
+    void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("name"));
+        Input input = new MockInput(new String[]{"0", String.valueOf(item.getId()), "1"});
+        UserAction[] actions = {
+                new DeleteAction(),
+                new ExitAction()
+        };
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findById(item.getId())).isNull();
+    }
+
+    @Test
+    void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("name"));
+        Input input = new MockInput(new String[]{"0", String.valueOf(item.getId()), "name1", "1"});
+        UserAction[] actions = {
+                new ReplaceAction(),
+                new ExitAction()
+        };
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("name1");
+
+    }
 }
 
